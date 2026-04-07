6f6cfa2403e038bac1452e1e598625cef1ebca58
HTML669 <appggj7@gmail.com>
2026-04-07 16:55:59 +0700

    Add files via upload

--- Project Structure/MyProject/server.js
+++ Project Structure/MyProject/server.js
@@ -48,3 +48,8 @@ app.get('/api/products/:id', (req, res) => {
 });
 
 app.listen(3000, () => console.log('🚀 Server ຣັນຢູ່ທີ່ http://localhost:3000'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
